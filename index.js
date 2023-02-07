9d2cf93554ba63f487b00dc62258dbfd57175972
Isabella Naranjo <63432808+isa-naranjo@users.noreply.github.com>
2023-02-07 08:12:09 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const num =[0,1,2,3,4,5]
+
+let x =15
+
+
+if(x%3 === 0 && x%5 === 0){
+    console.log("by both")
+} else if(x%3===0 ){
+    console.log("by3")
+} else if(x%5 === 0){
+    console.log("by5")
+} else {
+    console.log("No es divisible ni por 3 ni por 5")
+}
